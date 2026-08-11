f960da61386f41a1f04568ec630c646df445c131
Masashi Hanada <ad78ad78ad78ad78@gmail.com>
2026-08-11 17:05:44 +0900

    Initial Commit

---
+++ DR1101MK2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DR1101MK2
+//
+//  Created by Masashi Hanada on 2026/08/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DR1101MK2/DR1101MK2App.swift
@@ -0,0 +1,17 @@
+//
+//  DR1101MK2App.swift
+//  DR1101MK2
+//
+//  Created by Masashi Hanada on 2026/08/11.
+//
+
+import SwiftUI
+
+@main
+struct DR1101MK2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
